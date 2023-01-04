bdd01b2a11580ac387469137cfacc70b6d97f083
TheHardest18 <isias1626@gmail.com>
2023-01-04 13:02:47 -0400

    [UPD] module sale_custom_qty

--- sale_custom_qty/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ sale_custom_qty/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -20,7 +20,7 @@
     'version': '0.1',
     'licence': 'LGPL-3',
     # any module necessary for this one to work correctly
-    'depends': ['base', 'sale'],
+    'depends': ['base', 'sale', 'mrp'],
 
     # always loaded
     'data': [
